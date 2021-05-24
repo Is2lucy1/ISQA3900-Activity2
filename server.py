f85e37fcbbfd153a11ea885b45c0eee33fff2cf5
Lucy Chung <lchung@unomaha.edu>
2021-05-24 01:30:07 -0500

    scroll bar

--- server.py
+++ server.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+@app.route('/')
+def about():
+    return render_template('index.html')
 
 @app.route('/home')
-def about():
+def home():
     return render_template('index.html')
 
 @app.route('/projects')

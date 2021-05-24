92158e2ef5e342ff8c2f88e622696f6545cf1d7e
Lucy Chung <lchung@unomaha.edu>
2021-05-23 20:17:28 -0500

    First commit of basic pages

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/home')
+def about():
+    return render_template('home.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/experience')
+def resume():
+    return render_template('experience.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
